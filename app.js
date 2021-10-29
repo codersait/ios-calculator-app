07116a637c3cb47f5cfe9a9cca014e63662e8f59
codersait <codersait@gmail.com>
2021-10-29 23:02:16 +0300

    zero edge case solved

--- app.js
+++ app.js
@@ -1,74 +1,78 @@
-const currentDisplayField = document.querySelector('.current-display')
-const previousDisplayField = document.querySelector('.previous-display')
-const buttonsContainer = document.querySelector('.buttons-container')
+const currentDisplayField = document.querySelector('.current-display');
+const previousDisplayField = document.querySelector('.previous-display');
+const buttonsContainer = document.querySelector('.buttons-container');
 
-buttonsContainer.addEventListener('click', (e) => {
+buttonsContainer.addEventListener('click', e => {
   // numbers
   if (e.target.classList.contains('number') && currentDisplayField.innerText.length < 8) {
-    currentDisplayField.innerText += e.target.innerText
+    if ((currentDisplayField.innerText == '' && e.target.classList.contains('number-0')) || currentDisplayField.innerText.split('')[0] == 0) {
+      currentDisplayField.innerText = e.target.innerText;
+    } else {
+      currentDisplayField.innerText += e.target.innerText;
+    }
   }
   // decimal
   if (e.target.classList.contains('decimal') && !currentDisplayField.innerText.includes('.')) {
-    currentDisplayField.innerText += e.target.innerText
+    currentDisplayField.innerText += e.target.innerText;
   }
 
   // operands
   if (e.target.classList.contains('operator')) {
-    const operator = e.target.innerText
+    const operator = e.target.innerText;
     switch (operator) {
       case '+':
         if (currentDisplayField.innerText !== '') {
           previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
-          currentDisplayField.innerText = ""
+          currentDisplayField.innerText = '';
         }
         break;
       case '−':
         if (currentDisplayField.innerText !== '') {
           previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
-          currentDisplayField.innerText = ""
+          currentDisplayField.innerText = '';
         }
         break;
       case '×':
         if (currentDisplayField.innerText !== '') {
           previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
-          currentDisplayField.innerText = ""
+          currentDisplayField.innerText = '';
         }
         break;
       case '÷':
         if (currentDisplayField.innerText !== '') {
           previousDisplayField.innerText = `${currentDisplayField.innerText} ${operator}`;
-          currentDisplayField.innerText = ""
+          currentDisplayField.innerText = '';
         }
         break;
       case '=':
         if (currentDisplayField.innerText !== '') {
           if (previousDisplayField.innerText.includes('+')) {
-            const currentNumber = Number(currentDisplayField.innerText)
-            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0])
-            currentDisplayField.innerText = currentNumber + previousNumber
-            previousDisplayField.innerText = ''
+            const currentNumber = Number(currentDisplayField.innerText);
+            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0]);
+            currentDisplayField.innerText = currentNumber + previousNumber;
+            previousDisplayField.innerText = '';
           }
           if (previousDisplayField.innerText.includes('−')) {
-            const currentNumber = Number(currentDisplayField.innerText)
-            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0])
-            currentDisplayField.innerText = previousNumber - currentNumber
-            previousDisplayField.innerText = ''
+            const currentNumber = Number(currentDisplayField.innerText);
+            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0]);
+            currentDisplayField.innerText = previousNumber - currentNumber;
+            previousDisplayField.innerText = '';
           }
           if (previousDisplayField.innerText.includes('÷')) {
-            const currentNumber = Number(currentDisplayField.innerText)
-            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0])
+            const currentNumber = Number(currentDisplayField.innerText);
+            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0]);
             if (currentNumber === 0) {
-              currentDisplayField.innerText = 'Error'
+              currentDisplayField.innerText = 'Error';
             } else {
-              currentDisplayField.innerText = previousNumber / currentNumber
+              currentDisplayField.innerText = previousNumber / currentNumber;
             }
-            previousDisplayField.innerText = ''
+            previousDisplayField.innerText = '';
           }
           if (previousDisplayField.innerText.includes('×')) {
-            const currentNumber = Number(currentDisplayField.innerText)
-            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0])
-            currentDisplayField.innerText = previousNumber * currentNumber
-            previousDisplayField.innerText = ''
+            const currentNumber = Number(currentDisplayField.innerText);
+            const previousNumber = Number(previousDisplayField.innerText.split(' ')[0]);
+            currentDisplayField.innerText = previousNumber * currentNumber;
+            previousDisplayField.innerText = '';
           }
         }
         break;
@@ -79,26 +83,25 @@ buttonsContainer.addEventListener('click', (e) => {
   }
   // functions
   if (e.target.classList.contains('function')) {
-    const func = e.target.innerText
+    const func = e.target.innerText;
     switch (func) {
       case 'AC':
         //!dont use here innerText or textContent
-        currentDisplayField.innerText = ""
-        previousDisplayField.innerText = ""
+        currentDisplayField.innerText = '';
+        previousDisplayField.innerText = '';
         break;
       case '±':
         if (!currentDisplayField.innerText.includes('-')) {
-          currentDisplayField.prepend('-')
+          currentDisplayField.prepend('-');
         }
         break;
       case '%':
         // if (currentDisplayField.innerText !== '') {
         //   currentDisplayField.innerText = Number(currentDisplayField.innerText) / 100;
         // }
-        currentDisplayField.innerText = currentDisplayField.innerText.substring(0, currentDisplayField.innerText.length - 1)
+        currentDisplayField.innerText = currentDisplayField.innerText.substring(0, currentDisplayField.innerText.length - 1);
       default:
         break;
     }
   }
-
-})
+});
---
+++ test.js
@@ -0,0 +1,3 @@
+const str = '10.6 +';
+console.log(parseFloat(str));
+console.log(Number(str));
